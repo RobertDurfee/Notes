5d234aa95253d6b7a86d48dc0b2c514dd5660a09
Robert Durfee <robert.durfee.17@gmail.com>
2018-12-03 08:38:20 -0500

    removed bad code from 6.006 PS9P3

--- 18-19F/6.006/Problem Set/9/PS9P3.py
+++ 18-19F/6.006/Problem Set/9/PS9P3.py
@@ -2,7 +2,7 @@ from random import randrange
 from math import inf
 
 
-def max_district_2(A, d, i=0, j=None, memo={}):
+def max_district(A, d, i=0, j=None, memo={}):
 
     def value(a, b):
         if sum(A[a:b + 1]) >= (b - a + 1) / 2:
@@ -25,10 +25,10 @@ def max_district_2(A, d, i=0, j=None, memo={}):
     for k in range(i, j):
 
         if (i, k, 1) not in memo:
-            memo[(i, k, 1)] = max_district_2(A, 1, i, k)
+            memo[(i, k, 1)] = max_district(A, 1, i, k)
 
         if (k + 1, j, d - 1) not in memo:
-            memo[(k + 1, j, d - 1)] = max_district_2(A, d - 1, k + 1, j)
+            memo[(k + 1, j, d - 1)] = max_district(A, d - 1, k + 1, j)
 
         val1, seq1 = memo[(i, k, 1)]
         val2, seq2 = memo[(k + 1, j, d - 1)]
@@ -39,57 +39,14 @@ def max_district_2(A, d, i=0, j=None, memo={}):
 
     return mx
 
-def max_district_3(A, d, i=0, j=None, memo={}):
-
-    def value(a, b):
-        if sum(A[a:b + 1]) >= (b - a + 1) / 2:
-            return 1
-        else:
-            return 0
-
-    if j is None:
-        j = len(A) - 1
-
-    if d == 1:
-
-        if (j - i + 1) % 2 == 0:
-            return -inf, []
-        else:
-            return value(i, j), [(i, j)]
-
-    mx = (-inf, [])
-
-    for k in range(i, j):
-
-        if (i, k, 1) not in memo:
-            memo[(i, k, 1)] = max_district_3(A, 1, i, k)
-
-        if (k + 1, j, d - 1) not in memo:
-            memo[(k + 1, j, d - 1)] = max_district_3(A, d - 1, k + 1, j)
-
-        val1, seq1 = memo[(i, k, 1)]
-        val2, seq2 = memo[(k + 1, j, d - 1)]
-
-        vala, seqa = val1 + val2, seq1 + seq2
-
-        mx = max(mx, (vala, seqa), key=lambda x: x[0])
-
-    return mx
-
-
-# d = 3
-# A = [0, 1, 0, 1, 1, 0, 0]
 
 d = randrange(2, 10 + 1)
 A = [randrange(0, 1 + 1) for i in range(randrange(d * 2 + 1, 4 * d + 1, 2))]
+
 print(f'd = {d}')
 
 for i in range(len(A)):
 
     print()
-
     print(A[i:] + A[:i])
-
-    # print(max_district(A[i:] + A[:i], d))
-    print(max_district_2(A[i:] + A[:i], d))
-
+    print(max_district(A[i:] + A[:i], d))
